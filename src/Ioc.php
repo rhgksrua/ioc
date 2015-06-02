b7d0150280e0016dcdee1900b3d4203859ebad50
Han Koh <rhgksrua@gmail.com>
2015-06-02 12:12:23 -0500

    added comments to methods

--- src/Ioc.php
+++ src/Ioc.php
@@ -5,13 +5,17 @@ namespace Rhgksrua\Ioc;
 class Ioc
 {
     /**
-     *
+     * Contains closures to instantiate.
      */
     protected static $registry = array();
 
     /**
+     * Register closure.
      *
+     * This is where all the injections are kept.
      *
+     * @param string $name The name of object
+     * @param Closure $resolve Function that contains everything to instantiate.
      */
     public static function register($name, \Closure $resolve)
     {
@@ -19,9 +23,13 @@ class Ioc
     }
 
     /**
+     * Resolves required object.
      *
+     * @param string $name The name of object
+     * 
+     * @return instance Registered object
      *
-     *
+     * @throw Exception when requested name does not exist
      */
     public static function resolve($name)
     {
@@ -35,8 +43,11 @@ class Ioc
     }
 
     /**
+     * Returns true if object is in the container
      *
+     * @param string $name The name of object
      *
+     * @return bool name exists or it does not
      */
     public static function registered($name)
     {
